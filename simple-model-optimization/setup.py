a7fcf7ea24c036621729e120b34aae3423eb6630
carl <elt12cry@gmail.com>
2020-10-13 22:32:42 +0200

    Update

--- simple-model-optimization/setup.py
+++ simple-model-optimization/setup.py
@@ -1,18 +1,60 @@
+# from setuptools import find_packages, setup
+
+# INSTALL_REQUIRES = [
+#     'sklearn',
+#     'numpy',
+#     'pandas==1.1',
+#     'scipy',
+#     'gensim',
+# ]
+
+# setup(
+#     name='simple_model_optimization',
+#     packages=find_packages(where='src'),
+#     version='0.1.0',
+#     description='Python package for simple model optimization for demo of data-ml-workflow tools',
+#     author='Carl Rynegardh',
+#     license='MIT',
+#     install_requires=INSTALL_REQUIRES,
+#     package_dir={"": "src"},
+
+# )
+import codecs
+import os
+import re
+
 from setuptools import find_packages, setup
 
+###############################################################################
+
+NAME = "simple_model_optimization"
+PACKAGES = find_packages(where="src")
 INSTALL_REQUIRES = [
-    'sklearn',
-    'numpy',
-    'pandas',
-    'scipy'
+    "pandas",
+    "scikit-learn",
+    "matplotlib",
+    "pyyaml",
+    "joblib",
+    "sqlalchemy",
+    "attrs",
+    "scikit-optimize",
 ]
 
+META_PATH = os.path.join("src", NAME, "__init__.py")
+
+###############################################################################
+
+
+
+VERSION = "0.0.1"
+
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Python package for simple model optimization for demo of data-ml-workflow tools',
-    author='Carl Rynegardh',
-    license='MIT',
+    name=NAME,
+    version=VERSION,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    python_requires=">=3.7",
+    packages=PACKAGES,
     install_requires=INSTALL_REQUIRES,
+    package_dir={"": "src"},
 )
